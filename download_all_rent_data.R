2807e7b90c5b79ba19f2c687b737c1950c678c87
JBPOST2 <jbpost2@ncsu.edu>
2024-09-09 11:44:00 -0400

    Update data!

--- download_all_rent_data.R
+++ download_all_rent_data.R
@@ -6,10 +6,10 @@ library(tidycensus)
 state_abb <- datasets::state.name
 my_key <- "e267f117801b2ef741e54620602b0903c5f4d3c8"
 
-
-
 #rent variable name
-rent <- "DP04_0142PE"
+#rent <- "DP04_0142PE"
+#new variable as per Joey at census
+rent <- "B25071_001E"
 
 all_rent <- lapply(X = state_abb, FUN = function(x){
   get_acs(variables = rent, 
